41a545ab1885fc6a69b13503cb75e78067a28b48
Matsu moto <jaganbravo@gmail.com>
2019-06-30 07:11:14 -0400

    event

--- src/index.js
+++ src/index.js
@@ -1,27 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-function formatName(user){
-    return user.firstName + ' ' + user.lastName;
-}
-function greetingUser(user){
-    if(user){
-        return <h1>Hello, {formatName(user)}!</h1>
-    }else{
-        return <h1>Hello, Strager!</h1>
-    }
+
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-const user = {
-    firstName:'Jagan',
-    lastName:'Bravo'
-};
-const element = 
-    <div>
-        <h1>{greetingUser(user)}</h1>
-        <h2>Content is available</h2>
-    </div>
-
-ReactDOM.render(
-    element,
-    document.getElementById('root')
+function Avatar(props) {
+  return (
+    <img
+      className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
   );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+      <div className="UserInfo-age">{props.user.age}</div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+    age: 78,
+  },
+};
+// ReactDOM.render(
+//   <Comment
+//     date={comment.date}
+//     text={comment.text}
+//     author={comment.author}
+//   />,
+//   document.getElementById('root')
+// );
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
